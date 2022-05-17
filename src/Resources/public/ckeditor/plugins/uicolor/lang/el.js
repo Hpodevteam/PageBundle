c350e1cff4a8f7ab17c9c6f52e49fb0ca95e6185
Florian LE GARS <florian@hippocampe.fr>
2022-05-17 22:51:37 +0200

    remove ckeditor

--- src/Resources/public/ckeditor/plugins/uicolor/lang/el.js
+++
@@ -1,1 +0,0 @@
-CKEDITOR.plugins.setLang("uicolor","el",{title:"Διεπαφή Επιλογής Χρωμάτων",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"Προκαθορισμένα σύνολα χρωμάτων",config:"Επικολλήστε αυτό το κείμενο στο αρχείο config.js"});
