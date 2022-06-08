7d2c4dd88876dda97a614ab40c1ce7e109bb9c9b
Florian <legars.florian@gmail.com>
2022-06-08 15:47:52 +0200

    Merge (#5)
    * Feature/ux UI feature (#2)
    
    * evolutions
    
    * update README.md
    
    * Fix/supp (#4)
    
    * evolutions
    
    * update README.md
    
    * fix

--- src/Traits/SectionWidgetTab.php
+++ src/Traits/SectionWidgetTab.php
@@ -13,8 +13,8 @@ trait SectionWidgetTab
      * @ORM\OrderBy({"position" = "ASC"})
      * @ORM\ManyToMany(targetEntity="Hippocampe\Bundle\PageBundle\Entity\Section", cascade={"persist", "remove"}, orphanRemoval=true)
      * @ORM\JoinTable(name="section_widget_tab",
-     *      joinColumns={@ORM\JoinColumn(name="entity_id", referencedColumnName="id")},
-     *      inverseJoinColumns={@ORM\JoinColumn(name="section_id", referencedColumnName="id", unique=true)}
+     *      joinColumns={@ORM\JoinColumn(name="entity_id", referencedColumnName="id", onDelete="CASCADE")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="section_id", referencedColumnName="id", unique=true, onDelete="CASCADE")}
      *      )
      */
     private $sections;
